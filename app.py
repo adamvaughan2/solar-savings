7506c49ce41203ef86fe8da3b58d726cd570be8a
adamvaughan <151431693+adamvaughan2@users.noreply.github.com>
2025-06-21 10:08:27 +0100

    Change initial state of sidebar to expanded

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title='Solar Savings',
     page_icon="☀️",
     layout='wide',
-    initial_sidebar_state='auto',
+    initial_sidebar_state='expanded',
 )
 
 # dashboard title
